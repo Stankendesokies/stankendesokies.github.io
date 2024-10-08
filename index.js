ba5e71745c181cf0caabb686bfeb6e32b8f92aff
Stankendesokies <stankendesokies@gmail.com>
2024-10-08 16:31:20 +0200

    Arffffff
    I'm really stupid ok

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 function openNav() {
-    document.getElementByClass("sidebar").style.width = "200px";
+    document.getElementById("sidebar").style.width = "200px";
     document.getElementById("main_box").style.marginLeft = "200px";
     document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
   }
   
   function closeNav() {
-    document.getElementByClass("sidebar").style.width = "0";
+    document.getElementById("sidebar").style.width = "0";
     document.getElementById("main_box").style.marginLeft= "0";
     document.body.style.backgroundColor = "white";
   }
