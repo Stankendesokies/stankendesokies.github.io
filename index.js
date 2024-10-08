40e6de7361e7543d6ebd47af8c1dfcd5a6ea4add
Stankendesokies <stankendesokies@gmail.com>
2024-10-09 01:02:08 +0200

    Idk man, more side nav stuff

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 function openNav() {
-    document.getElementsByClassName("sidebar").style.width = "200px";
-    document.getElementsByClassName("main_box").style.marginLeft = "200px";
-    document.getElementsByClassName("top_bar").style.marginLeft = "200px";
+    document.getElementById("sidebar").style.width = "200px";
+    document.getElementById("main_box").style.marginLeft = "200px";
+    document.getElementById("top_bar").style.marginLeft = "200px";
     document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
   }
   
   function closeNav() {
-    document.getElementsByClassName("sidebar").style.width = "0";
-    document.getElementsByClassName("main_box").style.marginLeft= "0";
-    document.getElementsByClassName("top_bar").style.marginLeft = "0";
+    document.getElementById("sidebar").style.width = "0";
+    document.getElementById("main_box").style.marginLeft= "auto";
+    document.getElementById("top_bar").style.marginLeft = "auto";
     document.body.style.backgroundColor = "white";
   }
