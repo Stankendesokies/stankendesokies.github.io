4fb9ee4c71b44e50e9c771bde375e4f9a25a0a35
Stankendesokies <stankendesokies@gmail.com>
2024-10-09 01:44:17 +0200

    Woooo more colour changes

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 function openNav() {
     document.getElementById("sidebar").style.width = "200px";
-    document.body.style.backgroundColor = "#35373966";
-  }
+    document.body.style.backgroundColor = "#262729";
+}
   
   function closeNav() {
     document.getElementById("sidebar").style.width = "0";
     document.body.style.backgroundColor = "#36393E";
-  }
+}
