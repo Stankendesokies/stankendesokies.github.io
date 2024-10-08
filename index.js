e48e75673eab830c8333c07f871c816d9317d012
Stankendesokies <stankendesokies@gmail.com>
2024-10-09 01:14:10 +0200

    Decided to change up some stuff about the side bar

--- index.js
+++ index.js
@@ -1,13 +1,9 @@
 function openNav() {
     document.getElementById("sidebar").style.width = "200px";
-    document.getElementById("main_box").style.marginLeft = "200px";
-    document.getElementById("top_bar").style.marginLeft = "200px";
     document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
   }
   
   function closeNav() {
     document.getElementById("sidebar").style.width = "0";
-    document.getElementById("main_box").style.marginLeft= "auto";
-    document.getElementById("top_bar").style.marginLeft = "auto";
     document.body.style.backgroundColor = "white";
   }
