480f7c53a8ac093bcea88674dd6701254ad8ade7
Stankendesokies <stankendesokies@gmail.com>
2024-10-08 18:31:34 +0200

    Changing some stuff about the nav bar
    Idk, this shit seems to kinda suck ngl

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 function openNav() {
     document.getElementById("sidebar").style.width = "200px";
     document.getElementById("main_box").style.marginLeft = "200px";
+    document.getElementById("top_box").style.marginLeft = "200px";
     document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
   }
   
   function closeNav() {
     document.getElementById("sidebar").style.width = "0";
     document.getElementById("main_box").style.marginLeft= "0";
+    document.getElementById("top_box").style.marginLeft = "0";
     document.body.style.backgroundColor = "white";
   }
