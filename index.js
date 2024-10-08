9c686e68e4fae9bf5debafd49553d53ce7dc52f4
Stankendesokies <stankendesokies@gmail.com>
2024-10-08 19:43:40 +0200

    Fixing more bs
    ALSO "getElementsByClassName" is a thing
    
    My previous barking wasn't entirely justified

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 function openNav() {
-    document.getElementById("sidebar").style.width = "200px";
-    document.getElementById("main_box").style.marginLeft = "200px";
-    document.getElementById("top_bar").style.marginLeft = "200px";
+    document.getElementsByClassName("sidebar").style.width = "200px";
+    document.getElementsByClassName("main_box").style.marginLeft = "200px";
+    document.getElementsByClassName("top_bar").style.marginLeft = "200px";
     document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
   }
   
   function closeNav() {
-    document.getElementById("sidebar").style.width = "0";
-    document.getElementById("main_box").style.marginLeft= "auto";
-    document.getElementById("top_bar").style.marginLeft = "auto";
+    document.getElementsByClassName("sidebar").style.width = "0";
+    document.getElementsByClassName("main_box").style.marginLeft= "0";
+    document.getElementsByClassName("top_bar").style.marginLeft = "0";
     document.body.style.backgroundColor = "white";
   }
