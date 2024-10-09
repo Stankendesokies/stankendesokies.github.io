ed182fc60d0d876d1a4e5371a3654d09f31cab8b
Stankendesokies <stankendesokies@gmail.com>
2024-10-09 15:27:17 +0200

    The colour gradients in the JS were the wrong way around, whoopsie

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 function openNav() {
     document.getElementById("sidebar").style.width = "200px";
-    document.body.style.background = "linear-gradient(145deg, rgba(63,2,107,1) 0%, rgba(113,35,147,1) 25%, rgba(136,7,166,1) 50%, rgba(191,6,157,1) 75%, rgba(222,0,129,1) 100%)";
+    document.body.style.background = "linear-gradient(145deg, rgba(63,2,87,1) 0%, rgba(113,35,117,1) 25%, rgba(136,7,136,1) 50%, rgba(191,6,127,1) 75%, rgba(222,0,99,1) 100%)";
 }
   
   function closeNav() {
     document.getElementById("sidebar").style.width = "0";
-    document.body.style.background = "linear-gradient(145deg, rgba(63,2,87,1) 0%, rgba(113,35,117,1) 25%, rgba(136,7,136,1) 50%, rgba(191,6,127,1) 75%, rgba(222,0,99,1) 100%)";
+    document.body.style.background = "linear-gradient(145deg, rgba(63,2,107,1) 0%, rgba(113,35,147,1) 25%, rgba(136,7,166,1) 50%, rgba(191,6,157,1) 75%, rgba(222,0,129,1) 100%)";
 }
