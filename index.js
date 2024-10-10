6b02851cce949a0e2df5a99db16b610c16d38756
Stankendesokies <stankendesokies@gmail.com>
2024-10-10 14:33:15 +0200

    Testing audio stuff, also added soundgasm link to socials

--- index.js
+++ index.js
@@ -13,3 +13,10 @@ function openNav() {
     document.getElementById("sidebar").style.width = "0";
     document.body.style.background = "linear-gradient(145deg, rgba(63,2,107,1) 0%, rgba(113,35,147,1) 25%, rgba(136,7,166,1) 50%, rgba(191,6,157,1) 75%, rgba(222,0,129,1) 100%)";
 }
+
+function playing() {
+  document.getElementById("adio")
+    if (audio.currentTime >= 20000) {
+      alert("It's been 20 seconds");
+    }
+}
