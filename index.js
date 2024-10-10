e93dc630d1d15352d88eb3df27a84265f20f98e9
Stankendesokies <stankendesokies@gmail.com>
2024-10-10 03:58:02 +0200

    Made the side nav responsive to the width with JS

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 function openNav() {
+  if (window.innerWidth<=1350) {
+    document.getElementById("sidebar").style.width = "auto";
+    document.body.style.background = "linear-gradient(145deg, rgba(63,2,87,1) 0%, rgba(113,35,117,1) 25%, rgba(136,7,136,1) 50%, rgba(191,6,127,1) 75%, rgba(222,0,99,1) 100%)";
+  }
+  else {
     document.getElementById("sidebar").style.width = "200px";
     document.body.style.background = "linear-gradient(145deg, rgba(63,2,87,1) 0%, rgba(113,35,117,1) 25%, rgba(136,7,136,1) 50%, rgba(191,6,127,1) 75%, rgba(222,0,99,1) 100%)";
+  }
 }
   
   function closeNav() {
