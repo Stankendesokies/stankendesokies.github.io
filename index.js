5adc253f304ff8ffc5f4317e5cf0fc718567511a
Stankendesokies <stankendesokies@gmail.com>
2024-10-12 17:29:08 +0200

    Audio controls and time display were added

--- index.js
+++ index.js
@@ -36,12 +36,33 @@ document.addEventListener("DOMContentLoaded", function() {
   const videoPlayer = document.querySelector('.controls')
   const progress = videoPlayer.querySelector('.progress')
   const progressBar = videoPlayer.querySelector('.progress-bar')
+  const volume = videoPlayer.querySelector('.volume')
+  const currentTimeElement = videoPlayer.querySelector('.current')
+  const durationTimeElement = videoPlayer.querySelector('.duration')
+
   video.addEventListener('timeupdate', () => {
     const percentage = (video.currentTime / video.duration) * 100
     progressBar.style.width = `${percentage}%`
   })
+
   progress.addEventListener('click', (e => {
     const progressTime = (e.offsetX / progress.offsetWidth) * video.duration
     video.currentTime = progressTime
   }))
+
+  volume.addEventListener('mousemove', (e) => {
+    video.volume = e.target.value
+  })
+
+  const currentTime = () => {
+    let currentMinutes = Math.floor(video.currentTime / 60)
+    let currentSeconds = Math.floor(video.currentTime - currentMinutes * 60)
+    let durationMinutes = Math.floor(video.duration / 60)
+    let durationSeconds = Math.floor(video.duration - durationMinutes * 60)
+
+    currentTimeElement.innerHTML = `${currentMinutes}:${currentSeconds < 10 ? '0'+currentSeconds : currentSeconds}`
+    durationTimeElement.innerHTML = `${durationMinutes}:${durationSeconds}`
+  }
+
+  video.addEventListener('timeupdate', currentTime)
 });
