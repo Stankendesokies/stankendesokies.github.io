bcf41373d75815885705fb5653c764494fb5a6ba
Stankendesokies <stankendesokies@gmail.com>
2024-10-13 17:49:10 +0200

    Renamed some stuff and moved some files around to make it more readible

---
+++ sidenav.js
@@ -0,0 +1,16 @@
+function openNav() {
+  if (window.innerWidth<=1350) {
+    document.getElementById("sidebar").style.width = "410px";
+    document.body.style.background = "linear-gradient(145deg, rgba(89, 2, 124, 1) 0%, rgba(113,35,117,1) 25%, rgba(136,7,136,1) 50%, rgba(191,6,127,1) 75%, rgba(222,0,99,1) 100%)";
+  }
+  else {
+    document.getElementById("sidebar").style.width = "200px";
+    document.body.style.background = "linear-gradient(145deg, rgba(89, 2, 124, 1) 0%, rgba(113,35,117,1) 25%, rgba(136,7,136,1) 50%, rgba(191,6,127,1) 75%, rgba(222,0,99,1) 100%)";
+  }
+}
+  
+  function closeNav() {
+    document.getElementById("sidebar").style.width = "0";
+    document.body.style.background = "linear-gradient(145deg, rgba(63,2,107,1) 0%, rgba(113,35,147,1) 25%, rgba(136,7,166,1) 50%, rgba(191,6,157,1) 75%, rgba(222,0,129,1) 100%)";
+}
+
