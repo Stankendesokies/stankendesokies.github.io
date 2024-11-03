9d3c90b864947b9349eda66389500f133b457b38
Stankendesokies <72840051+Stankendesokies@users.noreply.github.com>
2024-11-03 21:20:49 +0100

    Nobody will notice this change
    Constant rgba values when changing background for nav bar

--- sidenav.js
+++ sidenav.js
@@ -1,11 +1,11 @@
 function openNav() {
   if (window.innerWidth<=1350) {
     document.getElementById("sidebar").style.width = "410px";
-    document.body.style.background = "linear-gradient(145deg, rgba(89, 2, 124, 1) 0%, rgba(113,35,117,1) 25%, rgba(136,7,136,1) 50%, rgba(191,6,127,1) 75%, rgba(222,0,99,1) 100%)";
+    document.body.style.background = "linear-gradient(145deg, rgba(93, 2, 124, 1) 0%, rgba(113,35,117,1) 25%, rgba(136,7,136,1) 50%, rgba(191,6,127,1) 75%, rgba(222,0,99,1) 100%)";
   }
   else {
     document.getElementById("sidebar").style.width = "200px";
-    document.body.style.background = "linear-gradient(145deg, rgba(89, 2, 124, 1) 0%, rgba(113,35,117,1) 25%, rgba(136,7,136,1) 50%, rgba(191,6,127,1) 75%, rgba(222,0,99,1) 100%)";
+    document.body.style.background = "linear-gradient(145deg, rgba(93, 2, 124, 1) 0%, rgba(113,35,117,1) 25%, rgba(136,7,136,1) 50%, rgba(191,6,127,1) 75%, rgba(222,0,99,1) 100%)";
   }
 }
   
