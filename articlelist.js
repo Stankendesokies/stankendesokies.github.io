f81868e423f37bcdbef0b4e0c31c3feb48a48cc8
Stankendesokies <stankendesokies@gmail.com>
2024-11-28 19:17:24 +0100

    Maybe this fixes it man idk

--- articlelist.js
+++ articlelist.js
@@ -1,15 +1,15 @@
-document.addEventListener("DOMContentLoaded", function() {
-    filterSelection("all")
-    function filterSelection(c) {
-    var x, i;
-    x = document.getElementsByClassName("filterDiv");
-    if (c == "all") c = "";
-    for (i = 0; i < x.length; i++) {
-        w3RemoveClass(x[i], "show");
-        if (x[i].className.indexOf(c) > -1) w3AddClass(x[i], "show");
-    }
-    }
+filterSelection("all")
+function filterSelection(c) {
+var x, i;
+x = document.getElementsByClassName("filterDiv");
+if (c == "all") c = "";
+for (i = 0; i < x.length; i++) {
+    w3RemoveClass(x[i], "show");
+    if (x[i].className.indexOf(c) > -1) w3AddClass(x[i], "show");
+}
+}
 
+document.addEventListener("DOMContentLoaded", function() {
     function w3AddClass(element, name) {
     var i, arr1, arr2;
     arr1 = element.className.split(" ");
