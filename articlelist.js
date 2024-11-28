47f824659a782e1767240832daec1a0896a6ffe9
Stankendesokies <stankendesokies@gmail.com>
2024-11-28 23:00:41 +0100

    FUUCCCKKKK

--- articlelist.js
+++ articlelist.js
@@ -30,15 +30,17 @@ function w3RemoveClass(element, name) {
   element.className = arr1.join(" ");
 }
 
-var btns = document.getElementsByClassName("btn");
-for (var i = 0; i < btns.length; i++) {
-    btns[i].addEventListener("click", function(){
-        var current = document.getElementsByClassName("active");
-        current[0].className = current[0].className.replace(" active", "");
-        this.className += " active";
-    });
-}
-
+document.addEventListener("DOMContentLoaded", function() {
+    var btns = document.getElementsByClassName("btn");
+    for (var i = 0; i < btns.length; i++) {
+        btns[i].addEventListener("click", function(){
+            var current = document.getElementsByClassName("active");
+            current[0].className = current[0].className.replace(" active", "");
+            this.className += " active";
+        });
+    }
+    filterSelection('all')
+});
 
 function myFunction() {
     var input, filter, ul, li, a, i, txtValue;
@@ -56,7 +58,3 @@ function myFunction() {
         }
     }
 }
-
-document.addEventListener("DOMContentLoaded", function() {
-    filterSelection('all')
-});
